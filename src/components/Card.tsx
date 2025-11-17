bd8ef0e33882423264283a4758422dc7956d499b
brookeclifton <brookeclifton9605@gmail.com>
2025-11-17 11:10:51 -1000

    Create Card.tsx

---
+++ src/components/Card.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import Link from 'next/link';
+import { Profile } from '@prisma/client';
+import { Card, Image } from 'react-bootstrap';
+
+const ProfileCard = ({ profile }: { profile: Profile }) => (
+  <Card className="h-100">
+    <Card.Header>
+      <Image src={profile.image} width={75} />
+      <Card.Title>
+        {profile.name}
+      </Card.Title>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>
+        {profile.description}
+        {profile.clean}
+        {profile.budget}
+        {profile.social}
+        {profile.study}
+        {profile.sleep}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <Link href="/messages">Chat</Link>
+    </Card.Footer>
+  </Card>
+);
+
+export default ProfileCard;
