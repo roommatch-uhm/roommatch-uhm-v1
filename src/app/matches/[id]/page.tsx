849c1396dab825df974c3303f1593f5a943f4bf6
Ethan Garcia <egarciaxx12@gmail.com>
2025-12-09 12:10:27 -1000

    Fix

--- src/app/matches/[id]/page.tsx
+++ src/app/matches/[id]/page.tsx
@@ -63,7 +63,9 @@ export default function profileDetailPage({ params }: { params: { id: string } }
     router.push(`/messages?chatId=${chat.id}`);
   };
 
-  //if (!profile) return <div>Loading...</div>;
+  if (!profile) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <Container className="py-4">
